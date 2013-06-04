cdaf5ed27108a669a8c3b5897efc139bf97c6d23
highlando <jnhlnd@gmail.com>
2013-06-05 00:08:04 +0200

    woe

--- tscr.py
+++ tscr.py
@@ -1,9 +1,11 @@
 class myclass():
 	def __init__(self, nummber):
+		from numpy import array
 		self.a = nummber
 		def incre(x):
 			return x+1
 		self.b = incre(self.a)
+		self.c = array([1])
 	
 
 
--- utests.py
+++ utests.py
@@ -14,53 +14,54 @@ class OptConPyFunctions(unittest.TestCase):
 		from sympy import diff, sin, simplify
 
 		self.mesh = UnitSquareMesh(24, 24)
-		self.V = VectorFunctionSpace(mesh, "CG", 2)
-		self.Q = FunctionSpace(mesh, "CG", 1)
+		self.V = VectorFunctionSpace(self.mesh, "CG", 2)
+		self.Q = FunctionSpace(self.mesh, "CG", 1)
 		self.nu = 1e-5
-		sel.F = Expression(('0','0'))
 
-		#x, y, t, nu, om = smp.symbols('x,y,t,nu,om')
-		#ft = smp.sin(om*t)
-		#u_x = ft*x*x*(1-x)*(1-x)*2*y*(1-y)*(2*y-1)
-		#u_y = ft*y*y*(1-y)*(1-y)*2*x*(1-x)*(1-2*x)
-		#p = ft*x*(1-x)*y*(1-y)
+		x, y, t, nu, om = smp.symbols('x,y,t,nu,om')
+		ft = smp.sin(om*t)
+		u_x = ft*x*x*(1-x)*(1-x)*2*y*(1-y)*(2*y-1)
+		u_y = ft*y*y*(1-y)*(1-y)*2*x*(1-x)*(1-2*x)
+		p = ft*x*(1-x)*y*(1-y)
 
-		#self.u_x = u_x
-		#self.u_y = u_y
-		#self.p = p
+		self.u_x = u_x
+		self.u_y = u_y
+		self.p = p
 
-		#def _sympy2expression(term):
-		#	'''Translate a SymPy expression to a FEniCS expression string.
-		#	'''
-		#	# This is somewhat ugly: 
-		#	# First replace the variables r, z, by something
-		#	# that probably doesn't appear anywhere else, 
-		#	# e.g., RRR, ZZZ, then
-		#	# convert this into a string, 
-		#	# and then replace the substrings RRR, ZZZ
-		#	# by x[0], x[1], respectively.
-		#	exp = smp.printing.ccode(term.subs('x','XXX').subs('y','YYY')) \
-		#		.replace('M_PI','pi') \
-		#		.replace('XXX','x[0]').replace('YYY','x[1]')
-		#	return exp
+		def sympy2expression(term):
+			'''Translate a SymPy expression to a FEniCS expression string.
+			'''
+			# This is somewhat ugly: 
+			# First replace the variables r, z, by something
+			# that probably doesn't appear anywhere else, 
+			# e.g., RRR, ZZZ, then
+			# convert this into a string, 
+			# and then replace the substrings RRR, ZZZ
+			# by x[0], x[1], respectively.
+			exp = smp.printing.ccode(term.subs('x','XXX').subs('y','YYY')) \
+				.replace('M_PI','pi') \
+				.replace('XXX','x[0]').replace('YYY','x[1]')
+			return exp
 
-		#dotu_x = simplify(diff(u_x,t))
-		#dotu_y = simplify(diff(u_y,t))
+		dotu_x = simplify(diff(u_x,t))
+		dotu_y = simplify(diff(u_y,t))
 
-		#diffu_x = simplify(nu*(diff(u_x,x,x) + diff(u_x,y,y)))
-		#diffu_y = simplify(nu*(diff(u_y,x,x) + diff(u_y,y,y)))
+		diffu_x = simplify(nu*(diff(u_x,x,x) + diff(u_x,y,y)))
+		diffu_y = simplify(nu*(diff(u_y,x,x) + diff(u_y,y,y)))
 
-		#dp_x = simplify( diff(p,x) )
-		#dp_y = simplify( diff(p,y) )
+		dp_x = simplify( diff(p,x) )
+		dp_y = simplify( diff(p,y) )
 
-		#adv_x = simplify( u_x*diff(u_x,x) + u_y*diff(u_x,y) )
-		#adv_y = simplify( u_x*diff(u_y,x) + u_y*diff(u_y,y) )
-		#
+		adv_x = simplify( u_x*diff(u_x,x) + u_y*diff(u_x,y) )
+		adv_y = simplify( u_x*diff(u_y,x) + u_y*diff(u_y,y) )
 
-        #self.fenics_sol_u = Expression((_sympy2expression(u_x),
-        #                           self._sympy2expression(u_y)),
-        #                           t=0.0 )
-        #self.fenics_sol_p = Expression(_sympy2expression(p), t=0.0)
+		self.F = Expression(('0','0'))
+		a = sympy2expression(u_x)
+		b = sympy2expression(u_x)
+		
+        self.fenics_sol_u = Expression((a,
+                                   b),
+                                   t=0.0, om=1.0)
 	
 	def test_testsolution_for_incompress(self):
 		from sympy import diff
