4cf1460d79a967bc4626344dcf6652457a388029
highlando <jnhlnd@gmail.com>
2013-06-19 18:38:11 +0200

    B done

--- test_inout_ops.py
+++ test_inout_ops.py
@@ -15,21 +15,8 @@ cdcoo = dict(xmin=0.4,
             ymin=0.2,
             ymax=0.3)
 
-cdom = ContDomain(cdcoo)
+cdom = cou.ContDomain(cdcoo)
 
-xv = np.arange(cdcoo['xmin'], cdcoo['xmax'], 
-                (cdcoo['xmax']-cdcoo['xmin'])/20)
+B = cou.get_inp_opa(cdom=cdom, V=V, NU=NU) 
 
-vv = np.zeros(2)
-bvv = np.zeros(2)
-
-u1 = L2abLinBas(3,NU)
-Bu = Inp2Rhs2D(u1, cdom)
-
-for x in xv:
-    u1.evaluate(vv, np.array([x]))
-    Bu.eval(bvv, np.array([x,0.25]))
-    print x, vv, bvv
-
-return u1, Bu, V
 
