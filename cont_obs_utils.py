4b9c56ea7e27f7514827706baa6624f14df97bc0
highlando <jnhlnd@gmail.com>
2013-06-20 20:49:02 +0200

    C mat still aliased

--- cont_obs_utils.py
+++ cont_obs_utils.py
@@ -12,11 +12,12 @@ def get_inp_opa(cdom=None, NU=8, V=None):
 
     BX, BY = [], []
 
-    for nbf in range(NU+1):
+    for nbf in range(NU):
         ubf = L2abLinBas(nbf, NU)
-        bux = Inp2Rhs2D(ubf, cdom, vcomp=0, xcomp=0)
-        buy = Inp2Rhs2D(ubf, cdom, vcomp=1, xcomp=0)
+        bux = Cast1Dto2D(ubf, cdom, vcomp=0, xcomp=0)
+        buy = Cast1Dto2D(ubf, cdom, vcomp=1, xcomp=0)
 
+        parameters["form_compiler_parameters"]["quadrature_degree"] = 1
         bx = inner(v,bux)*dx
         by = inner(v,buy)*dx
         Bx = assemble(bx)
@@ -30,30 +31,77 @@ def get_inp_opa(cdom=None, NU=8, V=None):
 
     return sps.hstack([sps.hstack(BX), sps.hstack(BY)])
 
+def get_mout_opa(odom=None, NY=8, V=None): 
+    """assemble the 'MyC' matrix
+
+    the find an array representation 
+    of the output operator 
+
+    the considered output is y(s) = 1/C int_x[1] v(s,x[1]) dx[1]
+    it is computed by testing computing my_i = 1/C int y_i v d(x)
+    where y_i depends only on x[1] and y_i is zero outside the domain
+    of observation. 1/C is the width of the domain of observation
+    cf. doku
+    """
+
+    v = TestFunction(V)
+    domains = CellFunction('uint', V.mesh())
+    domains.set_all(0)
+    odom.mark(domains, 1)
+
+    dx = Measure('dx')[domains]
+
+    Ci = 1.0/(odom.maxxy[0] - odom.minxy[0])
+
+    YX, YY = [], []
+
+    for nbf in range(NY):
+        ybf = L2abLinBas(nbf, NY, a=odom.minxy[1], b=odom.maxxy[1])
+        yx = Cast1Dto2D(ybf, odom, vcomp=0, xcomp=1)
+        yy = Cast1Dto2D(ybf, odom, vcomp=1, xcomp=1)
+        # if nbf == 1:
+        #     raise Warning('TODO: debug') 
+        # plot(yx, V.mesh())
+        yx = Ci*inner(v,yx)*dx(1)
+        yy = Ci*inner(v,yy)*dx(1)
+        Yx = assemble(yx)
+        Yy = assemble(yy)
+        Yx = Yx.array()
+        Yy = Yy.array()
+        Yx = Yx.reshape(1, len(Yx))
+        Yy = Yy.reshape(1, len(Yy))
+        YX.append(sps.csc_matrix(Yy))
+        YY.append(sps.csc_matrix(Yx))
+
+    My = ybf.massmat()
+
+    return sps.vstack([sps.vstack(YX), sps.vstack(YY)]), sps.block_diag([My,My])
 
 # Subdomains of Control and Observation
 class ContDomain(SubDomain):
     def __init__(self, ddict):
         SubDomain.__init__(self)
-        self.xmin, self.xmax = ddict['xmin'], ddict['xmax']
-        self.ymin, self.ymax = ddict['ymin'], ddict['ymax']
+        self.minxy = [ddict['xmin'], ddict['ymin']]
+        self.maxxy = [ddict['xmax'], ddict['ymax']]
     def inside(self, x, on_boundary):
-        return ( between(x[0], (self.xmin, self.xmax)) and
-                 between(x[1], (self.ymin, self.ymax)) )
+        epps = 3e-16
+        return ( between(x[0], (self.minxy[0]-epps, self.maxxy[0]+epps)) and
+                 between(x[1], (self.minxy[1]-epps, self.maxxy[1]+epps)) )
 
 class L2abLinBas():
     """ return the hat function related to the num-th vertex
 
     from the interval [a=0, b=1] with an equispaced grid
-    of N+1 inner vertices """
+    of N vertices """
 
     def __init__(self, num, N, a=0.0, b=1.0):
-        self.dist = (b-a)/N
-        self.vertex = num*self.dist
+        self.dist = (b-a)/(N-1)
+        self.vertex = a + num*self.dist
         self.num, self.N = num, N
         self.a, self.b = a, b
 
     def evaluate(self, s):
+        # print s
         if self.vertex - self.dist <= s <= self.vertex:
             sval = 1.0 - 1.0/self.dist*(self.vertex - s)
         elif self.vertex <= s <= self.vertex + self.dist:
@@ -62,11 +110,25 @@ class L2abLinBas():
             sval = 0
         return sval
 
-class Inp2Rhs2D(Expression):
-    """ map the control defined on [u.a, u.b]
+    def massmat(self):
+        """ return the mass matrix 
+        """
+        mesh = IntervalMesh(self.N-1, self.a, self.b)
+        Y = FunctionSpace(mesh, 'CG', 1)
+        yv = TestFunction(Y)
+        yu = TrialFunction(Y)
+        my = yv*yu*dx
+        my = assemble(my)
+        rows, cols, values = my.data()
+        return sps.csr_matrix((values, cols, rows))
+
+class Cast1Dto2D(Expression):
+    """ casts a function u defined on [u.a, u.b]
 
     into the f[comp] of an expression 
-    defined on the control domain cdom
+    defined on a 2D domain cdom by
+    by scaling to fit the xcomp extension
+    and simply extruding into the other direction
     """
 
     def __init__(self, u, cdom, vcomp=0, xcomp=0):
@@ -80,8 +142,8 @@ class Inp2Rhs2D(Expression):
         self.xcomp = xcomp
         # transformation of the intervals [cd.xmin, cd.xmax] -> [a, b]
         # via s = m*x + d
-        self.m = (self.u.b - self.u.a)/(cdom.xmax - cdom.xmin)
-        self.d = self.u.b - self.m*cdom.xmax
+        self.m = (self.u.b - self.u.a)/(cdom.maxxy[self.xcomp] - cdom.minxy[self.xcomp])
+        self.d = self.u.b - self.m*cdom.maxxy[self.xcomp]
 
     def eval(self, value, x):
         value[:] = 0
--- test_inout_ops.py
+++ test_inout_ops.py
@@ -1,22 +1,73 @@
 from dolfin import *
 import numpy as np
 import scipy.sparse as sps
+import scipy.sparse.linalg as spsla
 
 import cont_obs_utils as cou
+parameters.linear_algebra_backend = "uBLAS"
 
-N = 48 
+N = 40
 mesh = UnitSquareMesh(N, N)
 V = VectorFunctionSpace(mesh, "CG", 2)
 
-NU = 2
+NU, NY = 12, 14
 
 cdcoo = dict(xmin=0.4,
-            xmax=0.6,
-            ymin=0.2,
-            ymax=0.3)
+             xmax=0.6,
+             ymin=0.2,
+             ymax=0.3)
+
+odcoo = dict(xmin=0.45,
+             xmax=0.55,
+             ymin=0.6,
+             ymax=0.8)
 
 cdom = cou.ContDomain(cdcoo)
+odom = cou.ContDomain(odcoo)
 
-B = cou.get_inp_opa(cdom=cdom, V=V, NU=NU) 
+# get mass mat
+v, w = TrialFunction(V), TestFunction(V)
+Mv = assemble(inner(w,v)*dx)
+rows, cols, values = Mv.data()
+Mv = sps.csr_matrix((values, cols, rows))
 
+# # check the B
+# B = cou.get_inp_opa(cdom=cdom, V=V, NU=NU) 
+# 
+# # get mass mat
+# v, w = TrialFunction(V), TestFunction(V)
+# Mv = assemble(inner(w,v)*dx)
+# rows, cols, values = Mv.data()
+# Mv = sps.csr_matrix((values, cols, rows))
+# Bu = spsla.spsolve(Mv,B*np.vstack([0*np.ones((NU,1)),
+#                                    1*np.ones((NU,1))]))
+# 
+# v = Function(V)
+# v.vector().set_local(Bu)
+# plot(v)
 
+# check the C
+
+MyC, My = cou.get_mout_opa(odom=odom, V=V, NY=NY)
+
+exv = Expression(('1', '1'))
+testv = interpolate(exv, V)
+
+plot(testv)
+
+MyCv = MyC*testv.vector().array()
+testy = spsla.spsolve(My,MyCv)
+
+# print np.linalg.norm(testy)
+
+ymesh = IntervalMesh(NY-1, odcoo['ymin'], odcoo['ymax'])
+Y = FunctionSpace(ymesh, 'CG', 1)
+yx = Function(Y)
+yy = Function(Y)
+
+yx.vector().set_local(testy[:NY])
+plot(yx)
+yy.vector().set_local(testy[NY:])
+plot(yy)
+
+interactive(True)
