8f3fcc535d556f057d7941bed7e661dc21619fbe
highlando <jnhlnd@gmail.com>
2013-09-17 07:26:45 +0200

    clean up of the dir. splitting the utests.

---
+++ utests_solvutils.py
@@ -0,0 +1,45 @@
+import unittest
+import sympy as smp
+import numpy as np
+import scipy.sparse as sps
+import scipy.sparse.linalg as spsla
+
+# unittests for the helper functions
+
+class smw_formula(unittest.TestCase):
+
+    def setUp(self):
+
+        self.n = 500
+        self.k = 15
+        self.A = 30*sps.eye(self.n) + \
+                sps.rand(self.n, self.n, format='csr')
+        self.U = np.random.randn(self.n, self.k)
+        self.V = np.random.randn(self.k, self.n)
+        self.Z = np.random.randn(self.n, self.k+2)
+
+    def test_smw_formula(self):
+        """check the use of the smw formula
+
+        for the inverse of A-UV"""
+
+        import linsolv_utils as lsu
+
+        # check the branch with direct solves
+        AuvInvZ = lsu.app_smw_inv(self.A, U=self.U, V=self.V,
+                                    rhsa=self.Z, Sinv=None)
+        AAinvZ = self.A*AuvInvZ - np.dot(self.U, 
+                                    np.dot(self.V, AuvInvZ))
+        self.assertTrue(np.allclose(AAinvZ, self.Z))
+
+        #check the branch where A comes as LU
+        Alu = spsla.splu(self.A)
+        AuvInvZ = lsu.app_smw_inv(self.A, U=self.U, V=self.V,
+                                    rhsa=self.Z, Sinv=None)
+        AAinvZ = self.A*AuvInvZ - np.dot(self.U, 
+                                    np.dot(self.V, AuvInvZ))
+        self.assertTrue(np.allclose(AAinvZ, self.Z))
+
+
+if __name__ == '__main__':
+    unittest.main()
