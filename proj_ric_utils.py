verbose flag in adi iterations -- absolute tolerance for newton in adi because we cannot get better, cf. issue #9 -- iterative fnorm computation fixes #8

proj_ric_utils.py
@@ -113,10 +113,11 @@ def solve_proj_lyap_stein(A=None, J=None, W=None, M=None,
         try:
             if adi_dict['verbose']:
                 print ('Number of ADI steps {0} -- \n' + 
-                        'Relative norms of the update {1}'
+                        'Relative norm of the update {1}'
                             ).format(adi_step, rel_newZ_norm)
         except KeyError:
             pass # no verbosity specified - nothing is shown
+
 
     else:
         Z, atmtlu = _app_projinvz(W, At=At, Mt=Mt, J=J, ms=ms[0])
@@ -134,15 +135,18 @@ def solve_proj_lyap_stein(A=None, J=None, W=None, M=None,
             z_norm_sqrd = np.linalg.norm(Z)**2
             u_norm_sqrd = u_norm_sqrd + z_norm_sqrd
 
-            ufac = np.hstack([ufac, Z])
             rel_newZ_norm = np.sqrt(z_norm_sqrd/u_norm_sqrd)
 
             adi_step += 1
             adi_rel_newZ_norms.append(rel_newZ_norm)
 
-        print ('Number of ADI steps {0} -- \n' + 
-                'Relative norm of the update {1}'
-                    ).format(adi_step, rel_newZ_norm)
+        try:
+            if adi_dict['verbose']:
+                print ('Number of ADI steps {0} -- \n' + 
+                        'Relative norm of the update {1}'
+                            ).format(adi_step, rel_newZ_norm)
+        except KeyError:
+            pass # no verbosity specified - nothing is shown
 
     return dict(zfac=np.sqrt(-2*ms[0].real)*ufac,
                 adi_rel_newZ_norms=adi_rel_newZ_norms)

test_units_projriclyap.py
@@ -11,6 +11,8 @@ import proj_ric_utils as pru
 
 #### unittests for the helper functions
 
+verbose = False
+
 class TestProjLyap(unittest.TestCase):
 
     def setUp(self):
@@ -24,7 +26,7 @@ class TestProjLyap(unittest.TestCase):
                             nwtn_max_steps=24,
                             nwtn_upd_reltol=4e-8,
                             nwtn_upd_abstol=4e-8,
-                            verbose=False
+                            verbose=verbose
                                     )
 
         # -F, M spd -- coefficient matrices
@@ -54,11 +56,13 @@ class TestProjLyap(unittest.TestCase):
         # we need J sparse and of full rank
         for auxk in range(10):
             try:
-                self.J = sps.rand(self.NP, self.NV, density=0.03, format='csr')
+                self.J = sps.rand(self.NP, self.NV, 
+                        density=0.03, format='csr')
                 spsla.splu(self.J*self.J.T)
                 break
             except RuntimeError:
-                print 'J not full row-rank.. I make another try'
+                if verbose:
+                    print 'J not full row-rank.. I make another try'
         try:
             spsla.splu(self.J*self.J.T)
         except RuntimeError:
@@ -109,12 +113,10 @@ class TestProjLyap(unittest.TestCase):
                                     umat=U, vmat=V, 
                                     J=self.J, W=self.W,
                                     adi_dict=self.nwtn_adi_dict)['zfac']
-        print Z.shape
 
         Z2 = pru.solve_proj_lyap_stein(A=self.F-self.uvs, M=self.M, 
                                     J=self.J, W=self.W,
                                     adi_dict=self.nwtn_adi_dict)['zfac']
-        print Z2.shape
 
         Z3 = pru.solve_proj_lyap_stein(A=self.F.T-self.uvs.T, M=self.M.T, 
                                     J=self.J, W=self.W,
@@ -122,10 +124,10 @@ class TestProjLyap(unittest.TestCase):
                                     transposed=True)['zfac']
 
         Z4 = pru.solve_proj_lyap_stein(A=self.F.T, M=self.M.T, 
-                                        J=self.J, W=self.W,
-                                        umat=U, vmat=V, 
-                                        adi_dict=self.nwtn_adi_dict,
-                                        transposed=True)['zfac']
+                                    J=self.J, W=self.W,
+                                    umat=U, vmat=V, 
+                                    adi_dict=self.nwtn_adi_dict,
+                                    transposed=True)['zfac']
 
 ## TEST: {smw} x {transposed}
         self.assertTrue(np.allclose(Z,Z2))

tmptest_ric.py
@@ -5,7 +5,7 @@ import scipy.sparse.linalg as spsla
 
 import proj_ric_utils as pru
 
-Nv = 250
+Nv = 150
 Np = 40
 Ny = 5 
 nwtn_adi_dict = dict(
@@ -14,7 +14,7 @@ nwtn_adi_dict = dict(
             nwtn_max_steps=24,
             nwtn_upd_reltol=4e-8,
             nwtn_upd_abstol=4e-8,
-            verbosity=True
+            verbose=True
                     )
 
 # -F, M spd -- coefficient matrices
@@ -23,8 +23,9 @@ M = sps.eye(Nv) + sps.rand(Nv, Nv)*sps.rand(Nv, Nv)
 
 # right-handside: C= -W*W.T
 W = np.random.randn(Nv, Ny)
-U = np.random.randn(Nv, Ny)
-V = np.random.randn(Nv, Ny) 
+U = 1e-4*np.random.randn(Nv, Ny)
+V = np.random.randn(Nv, Ny).T
+uvs = sps.csr_matrix(np.dot(U, V))
 bmat = np.random.randn(Nv, Ny+2)
 
 # we need J sparse and of full rank
@@ -44,11 +45,21 @@ except RuntimeError:
 # MinvJt = lau.app_luinv_to_spmat(self.Mlu, self.J.T)
 # Sinv = np.linalg.inv(self.J*MinvJt)
 # self.P = np.eye(self.NV)-np.dot(MinvJt,Sinv*self.J)
+Z = pru.solve_proj_lyap_stein(A=F, M=M, 
+                            umat=U, vmat=V, 
+                            J=J, W=W,
+                            adi_dict=nwtn_adi_dict)['zfac']
+print Z.shape
 
-Z = pru.proj_alg_ric_newtonadi(mmat=M, fmat=F, jmat=J, bmat=bmat, 
-                            wmat=W, z0=bmat, 
-                            nwtn_adi_dict=nwtn_adi_dict,
-                            )['zfac']
+Z2 = pru.solve_proj_lyap_stein(A=F-uvs, M=M, 
+                            J=J, W=W,
+                            adi_dict=nwtn_adi_dict)['zfac']
+print Z2.shape
+
+# Z = pru.proj_alg_ric_newtonadi(mmat=M, fmat=F, jmat=J, bmat=bmat, 
+#                             wmat=W, z0=bmat, 
+#                             nwtn_adi_dict=nwtn_adi_dict,
+#                             )['zfac']
 
 # MtXM = M.T*np.dot(Z,Z.T)*M
 # MtXb = M.T*np.dot(np.dot(Z, Z.T), bmat)