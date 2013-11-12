3623bb127c137176be1c82ccf57e3f8a668c5911
highlando <jnhlnd@gmail.com>
2013-11-12 08:59:05 +0100

    back to lap

--- minex_subdomint.py
+++ minex_subdomint.py
@@ -9,26 +9,25 @@ class LocFun(dolfin.Expression):
         self.fvalue = fvalue
     def eval(self, value, x):
         value[:] = self.fvalue if self.xmin < x[0] < self.xmax else 0
-    def value_shape(self):
-        return (1,)
 
 class LocFuncDom(dolfin.SubDomain):
     """ domain of definition as subdomain """
     def __init__(self, xmin, xmax):
-        dolfin.SubDomain.__init__(self, xmin, xmax)
+        dolfin.SubDomain.__init__(self)
         self.xmin, self.xmax = xmin, xmax
     def inside(self, x, on_boundary):
         return (dolfin.between(x[0], (self.xmin, self.xmax)))
 
-smin, smax = 0.2, 0.3
+smin, smax = 0.1, 0.2
 
-mesh = dolfin.IntervalMesh(5, 1, 0)
+mesh = dolfin.IntervalMesh(11, 0, 1)
 dolfin.plot(mesh)
 
 Y = dolfin.FunctionSpace(mesh, 'CG', 1)
 
 y = dolfin.Expression('1')
 y = dolfin.interpolate(y, Y)
+dolfin.plot(y)
 
 domains = dolfin.CellFunction('uint', Y.mesh())
 domains.set_all(0)
@@ -36,6 +35,9 @@ domains.set_all(0)
 ffunc = LocFun(smin, smax)
 funcdom = LocFuncDom(smin, smax)
 funcdom.mark(domains, 1)
+
+dolfin.plot(ffunc, mesh)
+dolfin.interactive(True)
 
 y1 = inner(y, ffunc) * dx
 y2 = inner(y, ffunc) * dx(1)
