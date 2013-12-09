4dbcaba50b90eb1a98a5525834d81b72c97b5b4b
highlando <jnhlnd@gmail.com>
2013-12-09 13:01:13 +0100

    put things right in closed loop sim

--- optcont_main.py
+++ optcont_main.py
@@ -523,7 +523,7 @@ def optcon_nse(N=10, Nts=10):
     set_vpfiles(tip, fstring=('results/' + 'closedloop' + cntpstr +
                               'NewtonIt{0}').format(newtk))
 
-    old_v = 
+    v_old = inivalvec
     for t in np.linspace(tip['t0']+DT, tip['tE'], Nts):
 
         # t for implicit scheme
@@ -548,7 +548,7 @@ def optcon_nse(N=10, Nts=10):
 
         fvn = rhs_con[INVINDS, :] + rhsv_conbc + rhsd_vfstbc['fv']
         # rhsn = M*next_v + DT*(fvn + tb_mat * (tb_mat.T * next_w))
-        rhsn = M*old_v + DT*(fvn + 0*tb_mat * (tb_mat.T * next_w))
+        rhsn = M*v_old + DT*(fvn + 0*tb_mat * (tb_mat.T * next_w))
 
         amat = M + DT*(A + convc_mat)
         rvec = np.random.randn(next_zmat.shape[0], 1)
@@ -559,7 +559,7 @@ def optcon_nse(N=10, Nts=10):
 
         vpn = lau.app_smw_inv(amat, umat=-umate, vmat=vmate, rhsa=currhs)
         # vpn = np.atleast_2d(sps.linalg.spsolve(amat, currhs)).T
-        old_v = vpn[:NV]
+        v_old = vpn[:NV]
 
         yn = lau.apply_massinv(y_masmat, mc_mat*vpn[:NV])
         print 'current y: ', yn
